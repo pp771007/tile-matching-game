595d4fc862d643dd71bd31c62e430d92cbef7189
ckproduct <ckndim@gmail.com>
2024-08-05 15:09:04 +0800

    combo音效換成快樂頌

--- js/game.js
+++ js/game.js
@@ -16,7 +16,7 @@ const OBR_IMAGE_GROUPS = [
     ['11', '12', '13', '14', '15'],
     ['16', '17', '18', '19', '20'],
 ];
-
+let song = [3,3,4,5,5,4,3,2,1,1,2,3,3,2,2,3,3,4,5,5,4,3,2,1,1,2,3,2,1,1,2,2,3,1,2,3,4,3,1,2,3,4,3,2,1,2,3,1,1,3,3,4,5,5,4,3,2,1,1,2,3,2,1,1];
 function init() {
     stage = new createjs.Stage("gameCanvas");
     createjs.Touch.enable(stage);
@@ -752,8 +752,9 @@ function animateRemoval(matches) {
 
     let t = (combo - 1) % 16; // 週期為 16
     let soundId = t < 8 ? t + 1 : 16 - t;
-    createjs.Sound.play(`combo${soundId}`);
+    //createjs.Sound.play(`combo${soundId}`);
 
+    createjs.Sound.play(`combo${(song[song.length%combo])}`);
     for (let match of matches) {
         let orb = grid[match.y][match.x];
         grid[match.y][match.x] = null;
